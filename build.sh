9d34d6fc987119215e67f8473105351ac21debca
bt <bernhard.tellenbach@swisscyberstorm.com>
2015-12-15 15:49:57 +0100

    Build.sh file updated to do incremental update based on checksums

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ jekyll b
 rm -rv _site/build.sh
 #scp -rv _site/* www-data@isss.nine.ch:~/$DIR
 echo $DIR
-lftp -u www-data -e "mirror -R -c -e -n --ignore-time  _site $DIR; bye" sftp://isss.nine.ch
+#lftp -u www-data -e "mirror -R -c -e -n --ignore-time  _site $DIR; bye" sftp://isss.nine.ch
+rsync -vscrz -e "ssh  -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" _site/ www-data@isss.nine.ch:$DIR
